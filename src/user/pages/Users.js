e78ef382d3c51a725a08aab2accfcc97fa55c612
Mark Leo <ratrateroo@gmail.com>
2020-01-16 20:51:21 +0800

    getting users with get request

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+
 const Users = () => {
-    const USERS = [
-        {
-            id: 'u1',
-            name: 'Mark',
-            image: 'https://scontent.fmnl4-4.fna.fbcdn.net/v/t1.0-9/77154497_3178984255451306_5797013648283009024_n.jpg?_nc_cat=100&_nc_eui2=AeEbOCkj4mN9RQtOfObqzmNLQb_m1it0iLQpnbiRdfHBFWbY8TPOYkql2_2KqVoRcvi-eKVX8PGc_F3EIJYdZTugrsI-TGRDznyDLT3D8OP0aQ&_nc_ohc=nQP41bHsl34AQmqVXD3PTFVRhudDEcHLDYeIQ3d98Rq7RxCbuuGPzg6dQ&_nc_ht=scontent.fmnl4-4.fna&oh=6116a9f352c794e0cbe9b934b00afc0c&oe=5E74D275',
-            places: 3
-        },
-        {
-            id: 'u2',
-            name: 'Leo',
-            image: 'https://scontent.fmnl4-1.fna.fbcdn.net/v/t31.0-8/p720x720/414890_453414764674949_848983668_o.jpg?_nc_cat=104&_nc_eui2=AeFZgrnCD0yERe6aYZvPwNsZo32aDT6RGIDn5IaH_LdLQoiVdKA1PDO9WYQ67eIMyurc6VvxPw4h_meq2g-ULjG_MPIqV9ZK_ajPc6UFVM84vA&_nc_ohc=BIWkO0JrqP8AQnowYrI5mdXtRBdlikpREHJSCzTWA2uWn9qvOgKZ9_MxA&_nc_ht=scontent.fmnl4-1.fna&oh=3f9974eb16de0732757ca3c13c4575ab&oe=5E81A26B',
-            places: 1
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch('http://localhost:5000/api/users');
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
         }
-    ];
-    return <UsersList items={USERS} />;
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
